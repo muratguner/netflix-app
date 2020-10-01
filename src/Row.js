651161b32f3ed269893ba754b7d70fe9b65cc23a
Murat Güner <murat.guener@tum.de>
2020-10-01 13:05:18 +0300

    Movie trailer part was implemented

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -14,6 +18,34 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoPlay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    console.log(movie.original_name);
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          urlParams.get("v");
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  const _onReady = (event) => {
+    event.target.playVideo();
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -21,12 +53,16 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"} `}
             src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
           />
         ))}
       </div>
+      {trailerUrl && (
+        <Youtube videoId={trailerUrl} opts={opts} onReady={_onReady}></Youtube>
+      )}
     </div>
   );
 }
